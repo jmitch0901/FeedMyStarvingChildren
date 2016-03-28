2a28891ee3cec47f15f0e6e4cf71255aa2b98839
jmitch0901 <jmitch0901@gmail.com>
2016-03-28 15:45:22 +0000

    fixed routes and beginning to seed

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,21 @@
 var express = require("express"),
+    Path = require("path"),
     MainRouter = express.Router();
     
     
     
-// MainRouter.get('/',function(req,res){
+MainRouter.get('/',function(req,res){
     
-//     res.sendFile(__dirname+'/../public/login.html');
+    res.sendFile(__dirname+'/../public/login.html');
     
-// });
+});
 
 
 MainRouter.get("/img",function(req,res){
     
-    // Jimp.read(__dirname+'/img/spongebob.jpg')
-    // .then(function(pic){
-        
-    //     //console.log(pic);
-        
-    //     for(var i = 0; i < pic.bitmap.height; i++){
-    //         for(var j = 0; j < pic.bitmap.width; j++){
-    //             //console.log('uhhh');
-    //             pic.setPixelColor(0xFFFFFFFF,j,i);
-                
-    //         }
-    //     }
-        
-    //     pic.write(__dirname+'/img/white.jpg');
-        
-    // });
+
     
-    res.sendFile(__dirname+'/img/spongebob-small.jpg');
+    res.sendFile(Path.resolve(__dirname+'/../img/releasable-image.jpg'));
     
 });
 
