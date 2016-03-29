40f6b820f780c6fc7a8342a672f568e46df37e1c
jmitch <jmitch0901@gmail.com>
2016-03-29 16:12:24 -0500

    Use case for buying pixels is functional

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require("express"),
     Path = require("path"),
     PixelHandler = require("../factories/pixelHandlerv2"),
     passport = require("passport"),
+    Middleware = require('./middleware'),
     MainRouter = express.Router({mergeParams:true});
 
 
@@ -9,19 +10,13 @@ MainRouter.get("/login",function(req,res){
   res.json({message:"Login in by making POST request to /login.",isLoggedIn:req.isAuthenticated()});
 });
 
-//TODO
-//passport.authenticate("local",{failureRedirect:"/login"}),
 MainRouter.post('/login',passport.authenticate('local',{failureRedirect:'/login'}),function(req,res){
-  // if(req.isAuthenticated()){
-  //   res.status(406);
-  //   return res.json({error:"Please log out first!"});
-  // }
-  res.json({message:"Hello " + req.user.firstname+ ", you are now logged in."});
+  res.json({message:"Hello " + req.user.firstname+ ", you are now logged in.",isLoggedIn:req.isAuthenticated()});
 });
 
 MainRouter.get('/logout',function(req,res){
   req.logout();
-  res.redirect('/');
+  res.json({message:"You are now logged out.",isLoggedIn:req.isAuthenticated()})
 });
 
 var UserRoute = require("./user");
@@ -34,17 +29,30 @@ MainRouter.get('/',function(req,res){
     res.sendFile(__dirname+'/../public/login.html');
 });
 
+
+//TODO
+MainRouter.post('/buy',Middleware.isLoggedIn,function(req,res){
+
+  PixelHandler.buyPixels('100','suh dude',10000,function(err){
+      if(err){
+          console.log(err);
+      }
+      res.sendFile(Path.resolve(__dirname+'/../img/releasable-image.jpg'));
+  });
+});
+
 //TODO
 MainRouter.get("/img",function(req,res){
 
     console.log("Hitting /img");
-
-    PixelHandler.buyPixels('100','suh dude',1000,function(err){
-        if(err){
-            console.log(err);
-        }
-        res.sendFile(Path.resolve(__dirname+'/../img/releasable-image.jpg'));
-    });
+    //
+    // PixelHandler.buyPixels('100','suh dude',1000,function(err){
+    //     if(err){
+    //         console.log(err);
+    //     }
+    //     res.sendFile(Path.resolve(__dirname+'/../img/releasable-image.jpg'));
+    // });
+    res.sendFile(Path.resolve(__dirname+'/../img/releasable-image.jpg'));
 });
 
 
