eed4501933f03f0b0d320e25b2ad68c20109e634
jonathanmitchell <jmitch0901@gmail.com>
2016-04-01 14:40:29 -0500

    toggle working

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -3,8 +3,6 @@ angular.module('App')
   console.log("Home Controller Loaded");
 
   $scope.isLoggedIn = false;
-  $scope.showAbout = false;
-  $scope.showBuy = false;
   $scope.userName = "Jonathan Mitchell";
 
   $(document).ready(function(){
@@ -12,54 +10,98 @@ angular.module('App')
       $('#buy-slider').slideUp("fast");
   });
 
-  var firstAbout = true;
-  var firstBuy = true;
 
+  var aboutShown = false;
+  var buyShown = false;
 
-  $scope.$watch('showAbout',function(){
-    if(firstAbout){
-      firstAbout = false;
-      return;
-    }
-    $(document).ready(function(){
+  $scope.showAbout = function(){
+    console.log('showing about!');
+    aboutShown = !aboutShown;
 
-
-      if($scope.showBuy){
-        $('#buy-slider').slideUp("fast");
-      }
-
-
-      if(!$scope.showAbout){
-        console.log('slide up!');
-        $('#about-slider').slideUp("slow");
+    if(aboutShown){
+      if(buyShown){
+        $('#buy-slider').slideUp('fast',function(){
+          buyShown = false;
+          $('#about-slider').slideDown('slow');
+        });
       } else {
-        console.log('slide down!');
-        $('#about-slider').slideDown("slow");
+        $('#about-slider').slideDown('slow');
       }
-    });
-  });
-
-
-
-  $scope.$watch('showBuy',function(){
-    if(firstBuy){
-      firstBuy = false;
-      return;
+    } else {
+      $('#about-slider').slideUp('slow');
     }
-    $(document).ready(function(){
 
-      if($scope.showAbout){
-        $('#about-slider').slideUp("fast");
-      }
+  };
 
-      if(!$scope.showBuy){
-        console.log('slide up buy!');
-        $('#buy-slider').slideUp("slow");
+  $scope.showBuy = function(){
+    buyShown = !buyShown;
+
+    if(buyShown){
+      if(aboutShown){
+        $('#about-slider').slideUp('fast',function(){
+          aboutShown = false;
+          $('#buy-slider').slideDown('slow');
+        });
       } else {
-        console.log('slide down buy!');
-        $('#buy-slider').slideDown("slow");
+        $('#buy-slider').slideDown('slow');
       }
-    });
-  });
+    } else {
+        $('#buy-slider').slideUp('slow');
+    }
+
+  };
+
+
+  // var showAbout = false;
+  // var showBuy = false;
+  // var firstAbout = true;
+  // var firstBuy = true;
+  //
+  //
+  // $scope.$watch('showAbout',function(){
+  //   if(firstAbout){
+  //     firstAbout = false;
+  //     return;
+  //   }
+  //   $(document).ready(function(){
+  //
+  //
+  //     if($scope.showBuy){
+  //       $('#buy-slider').slideUp("fast");
+  //     }
+  //
+  //
+  //     if(!$scope.showAbout){
+  //       console.log('slide up!');
+  //       $('#about-slider').slideUp("slow");
+  //     } else {
+  //       console.log('slide down!');
+  //       $('#about-slider').slideDown("slow");
+  //     }
+  //   });
+  // });
+  //
+  //
+  //
+  // $scope.$watch('showBuy',function(){
+  //   if(firstBuy){
+  //     firstBuy = false;
+  //     return;
+  //   }
+  //   $(document).ready(function(){
+  //
+  //     if($scope.showAbout){
+  //       $('#about-slider').slideUp("fast");
+  //     }
+  //
+  //     if(!$scope.showBuy){
+  //       console.log('slide up buy!');
+  //       $('#buy-slider').slideUp("slow");
+  //     } else {
+  //       console.log('slide down buy!');
+  //       $('#buy-slider').slideDown("slow");
+  //     }
+  //   });
+  // });
 
 });
