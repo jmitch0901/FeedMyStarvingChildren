bc51d443bc2d9756c1a43334c73923ce1ff40809
jonathanmitchell <jmitch0901@gmail.com>
2016-04-01 15:48:57 -0500

    Log in for client

--- public/scripts/controllers/loginController.js
+++ public/scripts/controllers/loginController.js
@@ -1,9 +1,22 @@
 angular.module('App')
-.controller('LoginCtrl',function($location,$window){
+.controller('LoginCtrl',function($scope,$location,$window,UserFactory){
   console.log("Login Controller Added");
   // if ($location.protocol() !== 'https') {
   //   console.log('You are NOT going over https when you should!');
   //   // $window.location.href = $location.absUrl().replace('http', 'https');
   //   $window.location.href = 'https://localhost:3000/#/login'
   // }
+
+  $scope.email = "";
+  $scope.password = "";
+  $scope.login = function(){
+    //TODO finish this login method
+    UserFactory.login($scope.email,$scope.password,function(err){
+
+    });
+  };
+
+
+
+
 });
--- public/scripts/services/userFactory.js
+++ public/scripts/services/userFactory.js
@@ -4,9 +4,46 @@ angular.module('App')
 console.log('Initializing User Factory!');
 
   var user = {
+    me: undefined,
     isLoggedIn: false,
-    login: function(email,password){
+    login: function(email,password,callbacks){
+      var self = this;
+      $http({
+          method: 'POST',
+          url: "/api/login",
+          headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+          transformRequest: function(obj) {
+              var str = [];
+              for(var p in obj)
+              str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+              return str.join("&");
+          },
+          data: {email:email, password: password}
+      })
+      .success(function(result){
+        console.log("SUCCESS")
+        console.log(result);
+        $http.get('/api/me')
+        .success(function(result){
 
+          if(result.error){
+            console.log(result.error);
+            callbacks(result.error;)
+          } else {
+            self.me = result;
+            callbacks(null);
+
+          }
+
+        })
+        .error(function(result){
+          console.log(result);
+        });
+      })
+      .error(function(result){
+        console.log("ERROR");
+        console.log(result);
+      });
     },
     register: function(){
 
