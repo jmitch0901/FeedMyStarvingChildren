1a34c571e1e4a5a5e640e8c7f897619d0155fa95
jonathanmitchell <jmitch0901@gmail.com>
2016-04-23 18:24:37 -0500

    got good credit card validation

--- public/scripts/services/paypalFactory.js
+++ public/scripts/services/paypalFactory.js
@@ -8,13 +8,24 @@ angular.module('App')
       //ensure the user object is not null or empty object!
       console.log(user);
       $http.post('/api/user/'+user._id+'/buy',
-      {card:card,amount:amount,message:message},
-      {headers:{'Content-Type':'application/json'}}
+      {
+        card:card,amount:amount,message:message
+      },
+      {
+        headers:{'Content-Type':'application/json'}}
       )
       .then(function(result){
         console.log(result.data);
+        if(!result || !result.data){
+          console.error('There was an error purchasing:');
+          console.error(result);
+          return;
+        }
+
+        return callbacks(null,result.data);
       },function(err){
         console.error(err.data);
+        callbacks(err.data);
       });
     }
   };
